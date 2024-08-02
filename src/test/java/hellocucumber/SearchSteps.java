e3af564b765c105974025390662db7d80783f1cf
T. Tran <ttran375@my.centennialcollege.ca>
2024-08-02 08:59:02 +0000

    Using variables and examples

--- src/test/java/hellocucumber/SearchSteps.java
+++ src/test/java/hellocucumber/SearchSteps.java
@@ -15,9 +15,9 @@ public class SearchSteps {
     private String today;
     private String actualAnswer;
 
-    @Given("today is Sunday")
-    public void today_is_Sunday() {
-        today = "Sunday";
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
     }
 
     @When("I ask whether it's Friday yet")
@@ -28,10 +28,5 @@ public class SearchSteps {
     @Then("I should be told {string}")
     public void i_should_be_told(String expectedAnswer) {
         assertEquals(expectedAnswer, actualAnswer);
-    }
-
-    @Given("today is Friday")
-    public void today_is_Friday() {
-        today = "Friday";
     }
 }
